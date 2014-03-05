f0f90276bacf2a0979559b7ede63ea16259492f5
Kate Hudson <k88hudson@gmail.com>
2014-03-05 12:38:06 -0500

    Bug t978775: Add healthcheck

--- routes/devController.js
+++ routes/devController.js
@@ -4,6 +4,20 @@ module.exports = function(db) {
   var faker = new Faker();
 
   return {
+    healthcheck: function(env) {
+
+      var info = {
+        http: 'okay',
+        version: require('../package').version,
+        'ALLOWED_DOMAINS': env.get('ALLOWED_DOMAINS'),
+        'COOKIE_DOMAIN': env.get('COOKIE_DOMAIN')
+      };
+
+      return function (req, res) {
+        res.json(info);
+      };
+    },
+
     // Use with caution
     fake: function(req, res, next) {
 
