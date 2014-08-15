4db52bee0471cee7f40bf53a11279f9d6ecc7596
Aki Rose Braun <hi@akibraun.com>
2014-08-15 12:34:05 -0500

    Expose isEventPublic

--- models/event.js
+++ models/event.js
@@ -29,7 +29,8 @@ module.exports = function (sequelize, t) {
     'mentors',
     'tags',
     'flickrTag',
-    'makeApiTag'
+    'makeApiTag',
+    'isEventPublic'
   ];
 
   return sequelize.define('Event', {
